20008950b7c0eede3c46eae697cb52151cda5f80
Wilson McDade <ppiieemanonmc@gmail.com>
2023-04-08 11:30:12 -0400

    Initial commit

---
+++ DevcadeGame/Game1.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Devcade;
+
+// MAKE SURE YOU RENAME ALL PROJECT FILES FROM DevcadeGame TO YOUR YOUR GAME NAME
+namespace DevcadeGame
+{
+	public class Game1 : Game
+	{
+		private GraphicsDeviceManager _graphics;
+		private SpriteBatch _spriteBatch;
+		
+		/// <summary>
+		/// Stores the window dimensions in a rectangle object for easy use
+		/// </summary>
+		private Rectangle windowSize;
+		
+		/// <summary>
+		/// Game constructor
+		/// </summary>
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = false;
+		}
+
+		/// <summary>
+		/// Performs any setup that doesn't require loaded content before the first frame.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// Sets up the input library
+			Input.Initialize();
+
+			// Set window size if running debug (in release it will be fullscreen)
+			#region
+#if DEBUG
+			_graphics.PreferredBackBufferWidth = 420;
+			_graphics.PreferredBackBufferHeight = 980;
+			_graphics.ApplyChanges();
+#else
+			_graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+			_graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
+			_graphics.ApplyChanges();
+#endif
+			#endregion
+			
+			// TODO: Add your initialization logic here
+
+			windowSize = GraphicsDevice.Viewport.Bounds;
+			
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// Performs any setup that requires loaded content before the first frame.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+			// ex:
+			// texture = Content.Load<Texture2D>("fileNameWithoutExtension");
+		}
+
+		/// <summary>
+		/// Your main update loop. This runs once every frame, over and over.
+		/// </summary>
+		/// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			Input.Update(); // Updates the state of the input library
+
+			// Exit when both menu buttons are pressed (or escape for keyboard debugging)
+			// You can change this but it is suggested to keep the keybind of both menu
+			// buttons at once for a graceful exit.
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape) ||
+				(Input.GetButton(1, Input.ArcadeButtons.Menu) &&
+				Input.GetButton(2, Input.ArcadeButtons.Menu)))
+			{
+				Exit();
+			}
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// Your main draw loop. This runs once every frame, over and over.
+		/// </summary>
+		/// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+			
+			// Batches all the draw calls for this frame, and then performs them all at once
+			_spriteBatch.Begin();
+			// TODO: Add your drawing code here
+			
+			_spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Devcade-game-template
+A template repository for creating games for the Devcade arcade system.
+
+## Whats included
+
+- An initiallized monogame project
+- The Devcade library intalled
+- Resolution setting and basic controls updating
+- Build scripts for publishing and packing games
+
+### Getting started
+Making a game wth this template is very simple. First, make sure dotnet is installed on your local machine.
+Next, create a new repository using this template on GitHub, and clone your new repository.
+
+Once you have a local copy of the repository, open the project in your code editor of choice. 
+Make sure to rename all the `.sln` and `.csproj` file from `DevcadeGame` to the name of your game. After that, you are all set.
+
+A script is included to simplify this process. Run `./rename.sh YourGameName` or `./rename.sh CurrentGameName NewGameName` to rename your project.
+
+### Building with build scripts
+
+For Linux / Mac
+
+```sh
+. ./publish.bash [Path to banner] [Path to icon]
+```
+
+For Windows
+
+```sh
+./publish.ps1 [Path to banner] [Path to icon]
+```
+If arguments are not provided, the script will search for them in the top level of your project directory.
+
+In both cases, the banner and icon must be called 'banner' and 'icon' respectively. The scripts copy these files to your project directory, so running them without args after the first time will always find the banner and icon.
+
+The scripts will create a ZIP file in the project directory with the same name as the containing folder.
